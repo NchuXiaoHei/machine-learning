cb8336529cd9b11f628e0ffdb662c39b65747b57
xiaoyan.yuan <xiaoyan.yuan@hand-china.com>
2019-03-07 21:48:01 +0800

    添加项目文件。

---
+++ machine-learning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace machine_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
