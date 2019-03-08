ad5abafdd6d3002aceab11ba54789f79610a9ea8
xiaoyan.yuan <xiaoyan.yuan@hand-china.com>
2019-03-08 23:01:13 +0800

    f#版本的数据读取

---
+++ FSharp/Library.fs
@@ -0,0 +1,5 @@
+namespace FSharp
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ FSharp/Script1.fsx
@@ -0,0 +1,17 @@
+﻿open System.IO
+
+type Observation = {Label: string; Pixels: int[]}
+
+let toObservation (csvData: string) = 
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    {Label = label; Pixels = pixels}
+
+let reader (path:string) = 
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"D:\SojS\machine-learning\machine-learning-projects-for-dot-net-developers\chapter-1\DigitsRecognizer\Data\trainingsample.csv"
+let trainingData = reader trainingPath
